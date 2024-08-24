e3fc9669a61b6f443eb5baa7301ec37a5c7b8956
sofiquldev <sofiquldev@gmail.com>
2024-08-24 22:51:47 +0600

    update typo

--- app/components/SubscriberForm.jsx
+++ app/components/SubscriberForm.jsx
@@ -5,6 +5,7 @@ import styles from './styles/subscriber-form.module.scss';
 const SubscriberForm = () => {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [error, setError] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,12 +20,15 @@ const SubscriberForm = () => {
       const result = await res.json();
 
       if (res.ok) {
+        setError(false)
         setMessage('Thank you for subscribing!');
         setEmail('');
       } else {
+        setError(true)
         setMessage(result.error || 'Something went wrong. Please try again.');
       }
     } catch (error) {
+      setError(true)
       console.error('Error subscribing:', error);
       setMessage('Something went wrong. Please try again.');
     }
@@ -55,7 +59,7 @@ const SubscriberForm = () => {
         />
         <button type="submit">Subscribe</button>
       </div>
-      {message && <p>{message}</p>}
+      {message && <p className={ styles.subscribe__message } style={{ color: error ? '#cf4a0c': '#0b9756' }}>{message}</p>}
     </form>
   );
 };
